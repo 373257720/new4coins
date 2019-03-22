8ac17a5cecf8e3dcb2268d7c1246b200795014ba
tony <373257720@qq.com>
2019-03-22 15:17:27 +0800

    测试版

--- src/components/login.vue
+++ src/components/login.vue
@@ -79,10 +79,10 @@ export default {
         //   }
         // })
         this.$axios
-          .get("http://113.52.134.95:8080/growthing-02/users/pcLogin", {
+          .get("http://192.168.1.37:8080/growthing-02/users/pcLogin", {
             params: {
-              email: "this.name",
-              password: "this.password"
+              email: this.name,
+              password: this.password
             }
           })
           .then(res => {
